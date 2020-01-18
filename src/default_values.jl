84a93081a5a53e4ac113b3de07692faa4a4ae228
Dilum Aluthge <dilum@aluthge.com>
2020-01-18 01:03:19 -0500

    Update default_values.jl

--- src/default_values.jl
+++ src/default_values.jl
@@ -6,6 +6,8 @@ const _default_julia_version = "nightly"
 
 const _default_julia_cpu_target = "generic"
 
+const _recommended_julia_cpu_target = "generic"
+
 const _default_precompile_env_vars = Dict{String, String}("PREDICTMD_TEST_GROUP" => "all",
                                                           "PREDICTMD_TEST_PLOTS" => "true")
 
