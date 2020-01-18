afd265139b5e712a9e224e036c94d83fbc9181d8
Dilum Aluthge <dilum@aluthge.com>
2020-01-18 05:13:20 -0500

    Better default value for `JULIA_CPU_TARGET` (#23)

--- src/default_values.jl
+++ src/default_values.jl
@@ -1,12 +1,18 @@
+# import PackageCompilerX
+
 const _default_packagecompilerx_installation_command = "add https://github.com/KristofferC/PackageCompilerX.jl#master"
 
-const _future_packagecompilerx_installation_command = "add PackageCompilerX"
+# const _default_packagecompilerx_installation_command = "add PackageCompilerX"
 
 const _default_julia_version = "nightly"
 
-const _default_julia_cpu_target = "generic"
+# const _default_julia_cpu_target = PackageCompilerX.APP_CPU_TARGET
+# const _recommended_julia_cpu_target = PackageCompilerX.APP_CPU_TARGET
+# const _fastest_nonportable_julia_cpu_target = PackageCompilerX.NATIVE_CPU_TARGET
 
-const _recommended_julia_cpu_target = "generic"
+const _default_julia_cpu_target = "generic;sandybridge,-xsaveopt,clone_all;haswell,-rdrnd,base(1)"
+const _recommended_julia_cpu_target = _default_julia_cpu_target
+const _fastest_nonportable_julia_cpu_target = "native"
 
 const _default_precompile_env_vars = Dict{String, String}("PREDICTMD_TEST_GROUP" => "all",
                                                           "PREDICTMD_TEST_PLOTS" => "true")
