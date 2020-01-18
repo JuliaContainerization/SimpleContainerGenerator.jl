cba056ab072b41490595fa8b2b0a25359fd87f02
Dilum Aluthge <dilum@aluthge.com>
2020-01-18 01:12:37 -0500

    Update generate_packagecompiler.jl

--- src/generate_packagecompiler.jl
+++ src/generate_packagecompiler.jl
@@ -16,7 +16,7 @@ function _generate_packagecompiler_content(config::Config)
                   "import PackageCompilerX\n",
                   "PackageCompilerX.create_sysimage(",
                   "pkgnames_symbols; ",
-                  "cpu_target = \"$(julia_cpu_target)\", "
+                  "cpu_target = \"$(julia_cpu_target)\", ",
                   "precompile_execution_file=\"/opt/stopgapcontainers/precompile.jl\", ",
                   "sysimage_path = \"/opt/stopgapcontainers/sysimage/StopgapContainersSysimage.so\"",
                   ")")
